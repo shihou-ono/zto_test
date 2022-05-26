225840c6d598d02943a8314b26fcf99a91d6907c
shihou-ono <shi_ono@ap-com.co.jp>
2022-05-26 13:02:20 +0000

    complete

--- zto_bandwidth/zto_bandwidth.py
+++ zto_bandwidth/zto_bandwidth.py
@@ -2,14 +2,15 @@ from netmiko import ConnectHandler
 import textfsm
 import time
 import subprocess
+import datetime
 
 def measure_drop(net_connect):
   dropped = []
+  dir_path = "/home/ec2-user/yokogushi_contents_team/zto_bandwidth"
+  interface_queue_template = open(f"{dir_path}/vyos_interface_queue.textfsm", "r")
+  parser = textfsm.TextFSM(interface_queue_template)
   for i in range(2):
     output = net_connect.send_command("show interfaces ethernet eth1 queue")
-    interface_queue_template = open("./vyos_interface_queue.textfsm", "r")
-    parser = textfsm.TextFSM(interface_queue_template)
-    print(parser.ParseText(output))
     dropped.append(parser.ParseText(output)[1][1])
     time.sleep(3)
   return dropped
@@ -17,7 +18,8 @@ def measure_drop(net_connect):
 def get_initial_policy(net_connect):
   command = "show configuration commands | match 'eth1 traffic-policy in'"
   output = net_connect.send_command(command)
-  config_bandwidth_policy_template = open("./vyos_bandwidth_policy.textfsm", "r")
+  dir_path = "/home/ec2-user/yokogushi_contents_team/zto_bandwidth"
+  config_bandwidth_policy_template = open(f"{dir_path}/vyos_bandwidth_policy.textfsm", "r")
   parser = textfsm.TextFSM(config_bandwidth_policy_template)
   return parser.ParseText(output)[0][0]
 
@@ -54,48 +56,57 @@ def set_flag(net_connect, current_policy = ""):
 
   flag = is_dropped and bwp.not_max()
 
-  msg = f"is_dropped:{is_dropped},\n" \
-  f"current policy:{current_policy},\n" \
-  f"current_policy is not max:{bwp.not_max()},\n" \
-  f"need to increase bandwidth:{flag}\n"
+  msg = \
+  f"prev_dropped: {dropped[0]},\n" \
+  f"current_dropped: {dropped[1]},\n" \
+  f"prev_dropped < current_dropped: {is_dropped},\n" \
+  f"current policy: {current_policy},\n" \
+  f"current_policy is not max: {bwp.not_max()},\n" \
+  f"need to increase bandwidth: {flag}\n"
   print(msg)
   return flag
 
-vyos_router = {
-  "device_type": "vyos",
-  "host": "10.0.0.2",
-  "username": "vyos",
-  "password": "vyos",
-  "port": 22,
-  # "session_log": 'netmiko_session.log'
-}
+if __name__ == '__main__':
+  print("####")
+  print(datetime.datetime.now())
 
-net_connect = ConnectHandler(**vyos_router)
-time.sleep(3)
-
-current_policy = get_initial_policy(net_connect)
-
-flag = set_flag(net_connect, current_policy)
-
-while flag:
-  bwp = BandwidthPolicy(current_policy)
-  next_policy = bwp.next_policy()
-  config = [
-    f"delete traffic-policy limiter {current_policy} description", 
-    f"set traffic-policy limiter {next_policy} description current_policy",
-    f"set interfaces ethernet eth1 traffic-policy in {next_policy}",
-    f"set interfaces ethernet eth2 traffic-policy in {next_policy}"
-  ]
-
-  output = net_connect.send_config_set(config, exit_config_mode=False)
-  print(output)
-  time.sleep(3)
-  output = net_connect.commit()
-  print(output)
-  time.sleep(30)
-
-  current_policy = bwp.next_policy()
+  vyos_router = {
+    "device_type": "vyos",
+    "host": "10.0.0.2",
+    "username": "vyos",
+    "password": "vyos",
+    "port": 22,
+    # "session_log": 'netmiko_session.log'
+  }
 
   net_connect = ConnectHandler(**vyos_router)
   time.sleep(3)
+
+  current_policy = get_initial_policy(net_connect)
+
   flag = set_flag(net_connect, current_policy)
+
+  while flag:
+    bwp = BandwidthPolicy(current_policy)
+    next_policy = bwp.next_policy()
+    config = [
+      f"delete traffic-policy limiter {current_policy} description", 
+      f"set traffic-policy limiter {next_policy} description current_policy",
+      f"set interfaces ethernet eth1 traffic-policy in {next_policy}",
+      f"set interfaces ethernet eth2 traffic-policy in {next_policy}"
+    ]
+
+    output = net_connect.send_config_set(config, exit_config_mode=False)
+    print(output)
+    time.sleep(3)
+    output = net_connect.commit()
+    print(output)
+    time.sleep(30)
+
+    current_policy = bwp.next_policy()
+
+    net_connect = ConnectHandler(**vyos_router)
+    time.sleep(3)
+    flag = set_flag(net_connect, current_policy)
+
+  print("####")
